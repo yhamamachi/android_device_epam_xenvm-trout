7be89ca90be5d1a40e05734b2668581c484a1a3e
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-07-23 12:14:10 +0900

    xenvm_trout_arm64: select composer.ranchu APEX via BoardConfig
    Move the composer APEX selection out of the Xen domain config
    (doma-ironhide.cfg) and into the product build config. Android 15's
    trout base (via aosp_cf.mk -> auto/device_vendor.mk ->
    graphics/device_vendor.mk) only ships com.android.hardware.graphics.composer.ranchu;
    selecting drm_hwcomposer via bootarg pointed at an APEX that doesn't
    exist in this build, so composer3 HAL never registered in VINTF and
    SurfaceFlinger crash-looped.
    
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- xenvm_trout_arm64/BoardConfig.mk
+++ xenvm_trout_arm64/BoardConfig.mk
@@ -43,6 +43,7 @@ BOARD_BOOTCONFIG += androidboot.lcd_density=160
 BOARD_BOOTCONFIG += androidboot.hardware.hwcomposer=ranchu
 BOARD_BOOTCONFIG += androidboot.hardware.hwcomposer.mode=client
 BOARD_BOOTCONFIG += androidboot.hardware.hwcomposer.display_finder_mode=drm
+BOARD_BOOTCONFIG += androidboot.vendor.apex.com.android.hardware.graphics.composer=com.android.hardware.graphics.composer.ranchu
 BOARD_BOOTCONFIG += androidboot.hardware.gralloc=minigbm
 BOARD_BOOTCONFIG += androidboot.hardware.egl=mesa androidboot.boot_devices=33000000.pcie
 BOARD_BOOTCONFIG += androidboot.openthread_node_id=1
